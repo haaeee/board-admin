1794e227a677ec1a5a6f07c79c06c9d45d53768d
정종민 <jmchung@gabia.com>
2023-03-29 17:57:03 +0900

    init board-admin

---
+++ src/main/java/com/example/admintoyproject/AdminToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.admintoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/admintoyproject/AdminToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admintoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
